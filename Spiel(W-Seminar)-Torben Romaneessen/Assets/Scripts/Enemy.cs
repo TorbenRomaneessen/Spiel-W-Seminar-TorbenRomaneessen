8ff75b42c7d7b8327b41d2c1f9a2f5704b2415fe
TorbenRomaneessen <99121316+TorbenRomaneessen@users.noreply.github.com>
2022-02-07 14:01:04 +0100

    Enemy Animation

--- Spiel(W-Seminar)-Torben Romaneessen/Assets/Scripts/Enemy.cs
+++ Spiel(W-Seminar)-Torben Romaneessen/Assets/Scripts/Enemy.cs
@@ -8,9 +8,24 @@ public class Enemy : MonoBehaviour
     public int maxHealth = 100;
     int currentHealth;
 
+    public Transform enemy;
+    SpriteRenderer spriteRendererEnemy;
+    private float timePassed = 0;
+
+    public void Awake()
+    {
+        enemy = GetComponent<Transform>();
+        spriteRendererEnemy = GetComponent<SpriteRenderer>();
+    }
+
     void Start()
     {
         currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        EnemyMovement();
     }
 
     public void TakeDamage(int damage)
@@ -28,4 +43,29 @@ public class Enemy : MonoBehaviour
 
         Debug.Log("Enemy died!");
     }
+
+    private IEnumerator EnemyMovement()
+    {
+
+        if (timePassed < 1)
+        {
+            yield return new WaitForSeconds(1f);
+
+            this.transform.position = new Vector3(enemy.position.x + 0.0025f, enemy.position.y, enemy.position.z);
+            timePassed = timePassed + 1;
+            
+
+        }
+
+        else
+        {
+            yield return new WaitForSeconds(1f);
+            this.transform.position = new Vector3(enemy.position.x - 0.0025f, enemy.position.y, enemy.position.z);
+            timePassed = timePassed - 1;
+            this.transform.localScale = new Vector3(- enemy.localScale.x, enemy.localScale.y, enemy.localScale.z);
+
+
+        }
+    }
+
 }
