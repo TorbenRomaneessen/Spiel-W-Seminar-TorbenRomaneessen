5a87f08357511be1904e636d34cee3dc50147f89
TorbenRomaneessen <99121316+TorbenRomaneessen@users.noreply.github.com>
2022-04-19 18:32:08 +0200

    Added Main Theme Song

--- Spiel(W-Seminar)-Torben Romaneessen/Assets/Scripts/Game/AudioManager.cs
+++ Spiel(W-Seminar)-Torben Romaneessen/Assets/Scripts/Game/AudioManager.cs
@@ -37,6 +37,12 @@ public class AudioManager : MonoBehaviour
     }
 
 
+    private void Start()
+    {
+        Play("Theme");
+    }
+
+
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
