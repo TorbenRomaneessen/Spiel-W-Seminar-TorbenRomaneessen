850fc6750d5ec5834377023843dd0d63dc8f5993
TorbenRomaneessen <99121316+TorbenRomaneessen@users.noreply.github.com>
2022-08-01 12:01:55 +0200

    CleanUp (5) (working) + DashBug Fix

--- Spiel(W-Seminar)-Torben Romaneessen/Assets/Scripts/Game/Character.cs
+++ Spiel(W-Seminar)-Torben Romaneessen/Assets/Scripts/Game/Character.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour
@@ -8,32 +7,14 @@ public class Character : MonoBehaviour
     [SerializeField]
     private int _currentHealth = 3;
     private const float Speed = 6f;
+    private float _movementX;
     private const float JumpForce = 8f;
-
-    [Header("Attack")]
-    private const float AttackRange = 1.1f;
-    private const int AttackDamage = 1;
-    private const float AttackRate = 2f;
-    private float _nextAttackTime = 0f;
-    [SerializeField]
-    private Transform _attackPoint;
-    [SerializeField]
-    private LayerMask _enemyLayers;
-
+    public static bool IsDead;
     private float _invincibleTimer;
-
-    [Header("Dash")]
-    private float _dashTimer = 0f;
-    private const float DashingRate = 2f;
-    private const float DashingVelocity = 20f;
-    private const float DashingTime = 0.1f;
-    private bool _isDashing;
-    private bool _canDash;
-    private Vector2 _dashingDirection;
 
     [Header("Character Components")]
     [SerializeField]
-    private GameObject[] hearts;
+    private GameObject[] _hearts;
     private Transform _transform;
     private Rigidbody2D _rigidBody2D;
     private Animator _animator;
@@ -42,18 +23,30 @@ public class Character : MonoBehaviour
     [SerializeField]
     private ParticleSystem _dust;
 
-    public static bool IsDead;
+    [Header("Attack")]
+    private float _nextAttackTime;
+    private const float AttackRate = 2f;
+    private const float AttackRange = 1.1f;
+    private const int AttackDamage = 1;
+    [SerializeField]
+    private Transform _attackPoint;
+    [SerializeField]
+    private LayerMask _enemyLayers;
+
+    [Header("Dash")]
+    private float _nextDashTime;
+    private const float DashingRate = 2f;
+    private const float DashingVelocity = 20f;
+    private const float DashingTime = 0.1f;
+    private bool _isDashing;
+    private bool _canDash;
+    private Vector2 _dashingDirection;
+
+    [Header("Collision")]
+    private bool _takeDamage;
+    private bool _isGrounded;
 
 
-
-    private float _movementX;
-
-    private bool _collisionWithThorns;
-    private bool _collisionWithEnemy;
-    private bool _isGrounded = false;
-
-
-  
     public void Awake()
     {
         _rigidBody2D = GetComponent<Rigidbody2D>();
@@ -61,36 +54,69 @@ public class Character : MonoBehaviour
         _transform = GetComponent<Transform>();
     }
 
-
-    void Update()
+    private void Update()
     {
-        _invincibleTimer += Time.deltaTime;
-
-        CharacterRun();
-        AnimateWalk();
-        PlayerJump();
-        FlipCharacter();
+        Run();
+        AnimateRun();
+        TurnAround();
+        Jump();
         AttackCooldown();
-        DamageingObjects();
         Dash();
+        TakeDamage();
         CheckHearts();
     }
 
 
-    void CharacterRun()
+    private void Run()
     {
         _movementX = Input.GetAxisRaw("Horizontal");
-        transform.position += new Vector3(_movementX, 0f, 0f) * Time.deltaTime * Speed;
+        _transform.position += new Vector3(_movementX, 0f, 0f) * Time.deltaTime * Speed;
+    }
+
+    private void AnimateRun()
+    {
+        if (_movementX > 0 && _isGrounded)
+        {
+            _animator.SetBool("Run", true);
+            _animator.SetBool("Idle", false);
+            CreateDust();
+        }
+
+        if (_movementX == 0 && _isGrounded)
+        {
+            _animator.SetBool("Idle", true);
+            _animator.SetBool("Run", false);
+        }
+
+        if (_movementX < 0 && _isGrounded)
+        {
+            _animator.SetBool("Run", true);
+            _animator.SetBool("Idle", false);
+            CreateDust();
+        }
+    }
+
+    private void TurnAround()
+    {
+        if (_movementX < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        else if (_movementX > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
     }
 
 
-    void PlayerJump()
+    private void Jump()
     {
         if (Input.GetButtonDown("Jump") && _isGrounded)
         {
             _animator.SetTrigger("TakeOf");
-            _isGrounded = false;
             _rigidBody2D.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+            _isGrounded = false;
         }
 
         if (Input.GetButtonUp("Jump") && _rigidBody2D.velocity.y > 0)
@@ -109,105 +135,6 @@ public class Character : MonoBehaviour
         }
     }
 
- 
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            _isGrounded = true;
-            _canDash = true;
-        }
-
-        else
-        {
-            _isGrounded = false;
-        }
-
-        if (collision.gameObject.CompareTag("Thorns"))
-        {
-            _collisionWithThorns = true;
-            Debug.Log("Player has been hit");
-        }
-
-        else
-        {
-            _collisionWithThorns = false;
-        }
-
-        if(collision.gameObject.CompareTag("Coin"))
-        {
-            Destroy(collision.gameObject);
-        }
-
-        if(collision.gameObject.CompareTag("Enemy"))
-        {
-            _collisionWithEnemy = true;
-        }
-
-        else
-        {
-            _collisionWithEnemy = false;
-        }
-
-        if(collision.gameObject.CompareTag("Platform"))
-        {
-            _transform.parent = collision.transform;
-            _isGrounded = true;
-            _canDash = true;
-        }
-    }
-
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Platform"))
-        {
-            _transform.parent = null;
-        }
-    }
-
-
-    void AnimateWalk()
-    {
-        if (_movementX > 0 && _isGrounded)
-        {
-            _animator.SetBool("Run", true);
-            _animator.SetBool("Jump", false);
-            _animator.SetBool("Idle", false);
-            CreateDust();
-        }
-
-        if (_movementX == 0 && _isGrounded)
-        {
-            _animator.SetBool("Idle", true);
-            _animator.SetBool("Jump", false);
-            _animator.SetBool("Run", false);
-        }
-
-        if (_movementX < 0 && _isGrounded)
-        {
-            _animator.SetBool("Run", true);
-            _animator.SetBool("Jump", false);
-            _animator.SetBool("Idle", false);
-            CreateDust();
-        }
-    }
-
-
-    private void FlipCharacter()
-    {
-        if (_movementX < 0)
-        {
-            transform.eulerAngles = new Vector3(0, 180, 0);
-        }
-
-        else if (_movementX > 0)
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-        }
-    }
-
 
     private void Attack()
     {
@@ -216,6 +143,7 @@ public class Character : MonoBehaviour
             _animator.SetTrigger("Attack");
             FindObjectOfType<AudioManager>().Play("AttackSound (1)");
             FindObjectOfType<AudioManager>().Play("AttackSound (2)");
+
             Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(_attackPoint.position, AttackRange, _enemyLayers);
 
             foreach (Collider2D enemy in hitEnemies)
@@ -224,15 +152,6 @@ public class Character : MonoBehaviour
             }
         }
     }
-
-
-    private void OnDrawGizmosSelected()
-    {
-        if (_attackPoint == null)
-            return;
-        Gizmos.DrawWireSphere(_attackPoint.position, AttackRange);
-    }
-
 
     private void AttackCooldown()
     {
@@ -247,67 +166,34 @@ public class Character : MonoBehaviour
     }
 
 
-    public void CheckHearts()
-    {
-        if(_currentHealth < 1)
-        {
-            hearts[0].gameObject.SetActive(false);
-        }
-
-        else if(_currentHealth < 2)
-        {
-            hearts[1].gameObject.SetActive(false);
-        }
-
-        else if (_currentHealth < 3)
-        {
-            hearts[2].gameObject.SetActive(false);
-        }
-    }
-
-
-    public void DamageingObjects()
-    {
-        if ((_collisionWithThorns == true || _collisionWithEnemy == true) && _invincibleTimer >= 1)
-        {
-            FindObjectOfType<AudioManager>().Play("TakeDamageSound");
-            _animator.SetTrigger("TakeDamage");
-            _currentHealth -= 1;
-
-            Debug.Log("Player has been hit");
-            _invincibleTimer = 0;
-            transform.position = CheckPoint.ReachedPoint;
-        }
-
-        if (_currentHealth <= 0)
-        {
-            Debug.Log("Playerdied = true");
-            IsDead = true;
-            ScoreManager.instance2.ChangeDeathCounter();
-            _currentHealth = 3;
-        }
-
-    }
-
-
     private void Dash()
     {
-        if(Input.GetButtonDown("Dash") && _canDash && Time.time >= _dashTimer)
+        if (Input.GetButtonDown("Dash") && _canDash && Time.time >= _nextDashTime)
         {
             _animator.SetTrigger("Dash");
             FindObjectOfType<AudioManager>().Play("DashSound");
             CreateDashTrail();
+
             _isDashing = true;
             _canDash = false;
             _dashingDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-            if (_dashingDirection == Vector2.zero)
+            if (_dashingDirection == Vector2.zero )
             {
-                _dashingDirection = new Vector2(transform.localScale.x, 0);
+                // Checking in which direction the Character is turned.
+                if (_transform.rotation.y != 0)
+                {
+                    _dashingDirection = new Vector2(-1, 0);
+                }
+
+                else
+                {
+                    _dashingDirection = new Vector2(1, 0);
+                }
             }
 
-            StartCoroutine(nameof(StopDashing));
-            _dashTimer = Time.time + 1f / DashingRate;
+            StartCoroutine(nameof(StopDash));
+            _nextDashTime = Time.time + 1f / DashingRate;
         }
 
         if (_isDashing)
@@ -317,13 +203,54 @@ public class Character : MonoBehaviour
         }
     }
 
-
-    private IEnumerator StopDashing()
+    private IEnumerator StopDash()
     {
         yield return new WaitForSeconds(DashingTime);
         _isDashing = false;
         _rigidBody2D.velocity = _dashingDirection.normalized * 0;
     }
+
+
+    private void TakeDamage()
+    {
+        _invincibleTimer += Time.deltaTime;
+
+        if (_takeDamage == true && _invincibleTimer >= 1)
+        {
+            _animator.SetTrigger("TakeDamage");
+            FindObjectOfType<AudioManager>().Play("TakeDamageSound");
+
+            _currentHealth -= 1;
+            _invincibleTimer = 0;
+            transform.position = CheckPoint.ReachedPoint;
+        }
+
+        if (_currentHealth <= 0)
+        {
+            ScoreManager.instance2.ChangeDeathCounter();
+            IsDead = true;
+            _currentHealth = 3;
+        }
+    }
+
+    private void CheckHearts()
+    {
+        if (_currentHealth < 1)
+        {
+            _hearts[0].gameObject.SetActive(false);
+        }
+
+        else if (_currentHealth < 2)
+        {
+            _hearts[1].gameObject.SetActive(false);
+        }
+
+        else if (_currentHealth < 3)
+        {
+            _hearts[2].gameObject.SetActive(false);
+        }
+    }
+
 
     private void CreateDashTrail()
     {
@@ -333,5 +260,52 @@ public class Character : MonoBehaviour
     private void CreateDust()
     {
         _dust.Play();
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
+            _canDash = true;
+        }
+
+        else
+        {
+            _isGrounded = false;
+        }
+
+        // On entering the platform the character moves along the platform. Otherwise the character would fall off the platform.
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            _transform.parent = collision.transform;
+            _isGrounded = true;
+            _canDash = true;
+        }
+
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.CompareTag("Thorns") || collision.gameObject.CompareTag("Enemy"))
+        {
+            _takeDamage = true;
+        }
+
+        else
+        {
+            _takeDamage = false;
+        }
+    }
+
+    // On exiting the platform the characters movements are independent of the platforms one.
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            _transform.parent = null;
+        }
     }
 }
